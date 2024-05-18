152c4e222281e6d08d6a3ea6cbc08bbc6f57191f
Alexis <desert5@outlook.es>
2024-05-18 19:42:20 -0400

    UPDATE: defineEmits importation deleted

--- src/components/chat/UserProfile.vue
+++ src/components/chat/UserProfile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, defineEmits } from 'vue';
+import { ref } from 'vue';
 import type { User } from '../../interfaces/User.interface.ts';
 
 const props = defineProps<{
